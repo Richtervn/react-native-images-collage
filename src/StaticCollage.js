05e79bdd02b23e26b3e5430ef7b38e195d9094ac
Richtervn <tuanvuflp@gmail.com>
2018-12-07 13:24:00 +0700

    change to touchablewithoutfeedback

--- src/StaticCollage.js
+++ src/StaticCollage.js
@@ -1,46 +1,52 @@
 import React from 'react';
-import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Image, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import PropTypes from 'prop-types';
 
 class StaticCollage extends React.Component {
-  renderMatrix(){
-    const { matrix, direction, width, height, imageStyle, seperatorStyle, onPhotoPress, onPhotoPressIn, onPhotoPressOut } = this.props;
+  renderMatrix() {
+    const {
+      matrix,
+      direction,
+      width,
+      height,
+      imageStyle,
+      seperatorStyle,
+      onPhotoPress,
+      onPhotoPressIn,
+      onPhotoPressOut
+    } = this.props;
 
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    const sectionDirection = (direction == 'row') ? 'column' : 'row';
+    const sectionDirection = direction == 'row' ? 'column' : 'row';
 
     return matrix.map((element, m, array) => {
       const startIndex = m ? array.slice(0, m).reduce(reducer) : 0;
 
       const images = this.props.images.slice(startIndex, startIndex + element).map((image, i) => {
-        if(!onPhotoPress && !onPhotoPressIn && !onPhotoPressOut) {
-          return <Image key={i} source={{ uri: image }} style={[ { flex: 1 }, imageStyle ]} />
+        if (!onPhotoPress && !onPhotoPressIn && !onPhotoPressOut) {
+          return <Image key={i} source={{ uri: image }} style={[{ flex: 1 }, imageStyle]} />;
+        }
+        const touchProps = {};
+        if (onPhotoPress) {
+          touchProps.onPress = () => onPhotoPress({ uri: image, index: m*element + i });
+        }
+        if (onPhotoPressIn) {
+          touchProps.onPressIn = () => onPhotoPressIn({ uri: image, index: m*element + i });
+        }
+        if (onPhotoPressOut) {
+          touchProps.onPhotoPressOut = () => onPhotoPressOut({ uri: image, index: m*element + i });
         }
 
-      const touchProps = {};
-      if (onPhotoPress) {
-        touchProps.onPress = () => onPhotoPress({ uri: image, index: i })
-      }
-      if (onPhotoPressIn) {
-        touchProps.onPressIn = () => onPhotoPressIn({ uri: image, index: i })
-      }
-      if (onPhotoPressOut) {
-        touchProps.onPhotoPressOut = () => onPhotoPressOut({ uri: image, index: i })
-      }
-
-      return (
-          <TouchableOpacity
-            key={i}
-            {...touchProps} 
-            style={{flex: 1}}>
-            <Image source={{ uri: image }} style={[ { flex: 1 }, imageStyle ]} />
-          </TouchableOpacity>
+        return (
+          <TouchableWithoutFeedback key={i} {...touchProps} style={{ flex: 1 }}>
+            <Image source={{ uri: image }} style={[{ flex: 1 }, imageStyle]} />
+          </TouchableWithoutFeedback>
         );
       });
 
       return (
-        <View key={m} style={[ { flex: 1, flexDirection: sectionDirection }, seperatorStyle ]}>
-          { images }
+        <View key={m} style={[{ flex: 1, flexDirection: sectionDirection }, seperatorStyle]}>
+          {images}
         </View>
       );
     });
@@ -50,10 +56,8 @@ class StaticCollage extends React.Component {
     const { width, height, direction, containerStyle } = this.props;
 
     return (
-      <View style={[ { width, height }, containerStyle ]}>
-        <View style={{ flex: 1, flexDirection: direction }}>
-          { this.renderMatrix() }
-        </View>
+      <View style={[{ width, height }, containerStyle]}>
+        <View style={{ flex: 1, flexDirection: direction }}>{this.renderMatrix()}</View>
       </View>
     );
   }
@@ -61,26 +65,26 @@ class StaticCollage extends React.Component {
 
 StaticCollage.defaultProps = {
   // VARIABLES
-    direction: 'row',
+  direction: 'row',
   // STYLE OF SEPERATORS ON THE COLLAGE
-    seperatorStyle: {
-      borderWidth: 2,
-      borderColor: 'white',
-    },
+  seperatorStyle: {
+    borderWidth: 2,
+    borderColor: 'white'
+  },
 
   // STYLE
   containerStyle: {
     borderWidth: 4,
     borderColor: 'black',
-    backgroundColor: 'white',
+    backgroundColor: 'white'
   },
-  imageStyle: {}, // DEFAULT IMAGE STYLE
+  imageStyle: {} // DEFAULT IMAGE STYLE
 };
 
 StaticCollage.propTypes = {
   images: PropTypes.array,
   matrix: PropTypes.array,
-  direction: PropTypes.oneOf(['row', 'column']),
+  direction: PropTypes.oneOf(['row', 'column'])
 };
 
 export { StaticCollage };
